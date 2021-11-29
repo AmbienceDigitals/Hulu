4604054ac064705656338107830409fa8a14f07d
Ayano Ezekiel <52824649+AmbienceDigitals@users.noreply.github.com>
2021-11-29 10:29:39 +0100

    fully functional with flip effect

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ import HeaderItem from "./HeaderItem";
 
 function Header() {
     return (
-        <header className='flex flex-col'>
-            <div className='flex flex-grow justify-evenly max-w-2xl rounded-'>
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
                 <HeaderItem
                 title='HOME'
                 Icon={HomeIcon}/>
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,7 +3,7 @@ function HeaderItem({title, Icon}) {
         <div className='flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover: text-white l'>
             <Icon
             className='h-8 mb-1 group-hover:animate-bounce'/>
-            <p className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</p>
+            <p className='opacity-0 group-hover:opacity-100 tracking-widest border'>{title}</p>
         </div>
     )
 }
